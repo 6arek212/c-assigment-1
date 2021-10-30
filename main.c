44167c59bb2455e45de5a2289e6beba88ca5733c
tarik <tarik.id.10@hotmail.com>
2021-10-30 23:44:37 +0300

    ver5

--- main.c
+++ main.c
@@ -23,39 +23,39 @@ int main()
         end = x;
     }
 
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for (int i = start; i <= end; i++)
     {
-        if (isArmstrong(i))
+        if (isArmstrong(i) && i < end)
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
 
-    printf("\nThe Palindromes are: ");
+    printf("\nThe Palindromes are:");
     for (int i = start; i <= end; i++)
     {
         if (isPalindrome(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
 
-    printf("\nThe Prime numbers are: ");
+    printf("\nThe Prime numbers are:");
     for (int i = start; i <= end; i++)
     {
         if (isPrime(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
 
-    printf("\nThe Strong numbers are: ");
+    printf("\nThe Strong numbers are:");
     for (int i = start; i <= end; i++)
     {
         if (isStrong(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
